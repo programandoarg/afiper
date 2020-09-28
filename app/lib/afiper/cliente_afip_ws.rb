f1fc516d4ec47f89cb469448d2af6b50be832985
Martín Rosso <mrosso10@gmail.com>
2020-09-27 21:22:50 -0300

    refactor ClienteAfipWs

--- app/lib/afiper/cliente_afip_ws.rb
+++ app/lib/afiper/cliente_afip_ws.rb
@@ -1,62 +1,78 @@
 # frozen_string_literal: true
 
 require 'savon'
-require 'afiper/errors/wsfe_client_error'
 
 module Afiper
   # WSAA: Autenticación para todos los web services de AFIP
   class ClienteAfipWs
-    def initialize(contribuyente)
-      @contribuyente = contribuyente
+    def initialize(options = {})
+      @contribuyente = options[:contribuyente]
+      @service_name = options[:service_name]
+      @service_url = options[:service_url]
     end
 
     def homologacion
       unless Afiper.configuration.wsfe_homologacion.in?([true, false])
-        raise 'Afiper: Error de configuración' \
-              'Agregar config/initializers/afiper.rb' \
-              '  Afiper.configure do |config|' \
-              '    config.wsfe_homologacion = true' \
-              '  end'
+        raise Error, 'Afiper: Error de configuración' \
+                     'Agregar config/initializers/afiper.rb' \
+                     '  Afiper.configure do |config|' \
+                     '    config.wsfe_homologacion = true' \
+                     '  end'
       end
 
       Afiper.configuration.wsfe_homologacion
     end
 
-    def call(method, params = {})
-      client = build_client
+    def call_auth(method, params = {})
       message = { Auth: auth_hash }
-      response = client.call(method, message: message.deep_merge(params))
-      # puts response.as_json
-      # puts %("response.body[:"#{method}_response"].present? && response.body[:"#{method}_response"][:"#{method}_result"].present?")
-      if response.body[:"#{method}_response"].present? && response.body[:"#{method}_response"][:"#{method}_result"].present?
+      response = call_raw(@service_url, method, message.deep_merge(params))
+
+      if response.body[:"#{method}_response"].present? &&
+         response.body[:"#{method}_response"][:"#{method}_result"].present?
         response = response.body[:"#{method}_response"][:"#{method}_result"]
         if response[:errors].present?
-          messages = response[:errors][:err]
-          raise Afiper::Errors::WsfeClientError, [messages].flatten.to_json
+          errores = response[:errors][:err]
+          raise AfipWsError, errores
         end
         response
       else
-        raise Afiper::Errors::WsfeClientError, [{ code: 0, msg: 'Error en el Web Service de la AFIP' }].to_json
+        raise ErrorTemporal, 'Error en el Web Service de la AFIP'
       end
-    rescue SocketError => e
-      raise Afiper::Errors::WsfeClientError, [{ code: 90000111, message: 'Error de conexión' }].to_json
+    end
+
+    def timeout_errors
+      excs = [HTTPI::TimeoutError]
+      defined?(HTTPClient) && excs << HTTPClient::ReceiveTimeoutError
+      excs
     end
 
     def auth_hash
       unless @auth_hash.present?
-        token = WsaaToken.where(contribuyente: @contribuyente, service: service_name, homologacion: homologacion).where('created_at > ?', Time.zone.now - 6.hours).first
+        token = WsaaToken.where(contribuyente: @contribuyente, service: @service_name, homologacion: homologacion).where('created_at > ?', Time.zone.now - 6.hours).first
         unless token.present?
           new_token = wsaa
-          token = WsaaToken.create(contribuyente: @contribuyente, service: service_name, cuit: @contribuyente.cuit, homologacion: homologacion, token: new_token[0], sign: new_token[1])
+          token = WsaaToken.create(contribuyente: @contribuyente, service: @service_name, cuit: @contribuyente.cuit, homologacion: homologacion, token: new_token[0], sign: new_token[1])
         end
         @auth_hash = token.auth_hash
       end
       @auth_hash
     end
 
+    def call_raw(url, method, message)
+      client = Savon.client do
+        wsdl url
+        convert_request_keys_to :none
+      end
+      response = client.call(method, message: message)
+    rescue *timeout_errors
+      raise ErrorTemporal, 'La solicitud tardó demasiado en completarse'
+    rescue SocketError
+      raise ErrorTemporal, 'Error de conexión'
+    end
+
     def wsaa
       unless @contribuyente.afip_configured?(homologacion)
-        raise 'Los certificados de la AFIP no están configurados'
+        raise Error, 'Los certificados de la AFIP no están configurados'
       end
 
       if homologacion
@@ -74,12 +90,9 @@ module Afiper
       signed = OpenSSL::PKCS7.sign certificate, key, generar_tra
       signed = signed.to_pem.lines.to_a[1..-2].join
 
-      client = Savon.client do
-        wsdl url
-        convert_request_keys_to :none # or one of [:lower_camelcase, :upcase, :none]
-      end
-      response = client.call(:login_cms, message: { in0: signed })
+      response = call_raw(url, :login_cms, in0: signed)
       ta = Nokogiri::XML(Nokogiri::XML(response.to_xml).text)
+
       [ta.css('token').text, ta.css('sign').text]
     end
 
@@ -92,23 +105,15 @@ module Afiper
         xml.header do
           xml.uniqueId Time.now.to_i
           xml.generationTime xsd_datetime Time.now - ttl
-          # TODO: me parece que no le da mucha bola el WS al expirationTime
+          # me parece que no le da mucha bola el WS al expirationTime
           xml.expirationTime xsd_datetime Time.now + ttl
         end
-        xml.service service_name
+        xml.service @service_name
       end
     end
 
     def xsd_datetime(time)
       time.strftime('%Y-%m-%dT%H:%M:%S%z').sub(/(\d{2})(\d{2})$/, '\1:\2')
-    end
-
-    def build_client
-      url = service_url
-      client = Savon.client do
-        wsdl url
-        convert_request_keys_to :none
-      end
     end
   end
 end
